90b5490cb2a3309bf9a8c0f9f82ded7c3419ba77
Jen Yuan <jenyuan@me.com>
2025-03-25 22:30:51 +0100

    Misc

---
+++ src/app/(pages)/paris/_components/LocationsSection.js
@@ -0,0 +1,68 @@
+'use client';
+
+import React, { useEffect, useRef } from 'react';
+import useAsideMap from '@/hooks/useAsideMap';
+import CategoryPill from '@/components/CategoryPill';
+import LocationCard from '@/components/LocationCard';
+import Button from '@/components/Button';
+
+export default function LocationsSection({ data }) {
+	const { locationList, locationCategories } = data || {};
+	const locationRef = useRef(null);
+	const setAsideMapActive = useAsideMap((state) => state.setAsideMapActive);
+	const setAsideMapLocations = useAsideMap(
+		(state) => state.setAsideMapLocations
+	);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				setAsideMapActive(entry.isIntersecting);
+			},
+			{ threshold: 0.5 } // Trigger when 50% of the component is visible
+		);
+
+		if (locationRef.current) {
+			observer.observe(locationRef.current);
+		}
+
+		return () => observer.disconnect();
+	}, []);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setAsideMapLocations(locationList);
+		}, 1);
+	}, [locationList]);
+
+	if (!locationList) return null;
+
+	return (
+		<section ref={locationRef} className="p-paris__locations">
+			<div className="p-paris__locations__grid">
+				{locationList.map((item, index) => (
+					<LocationCard key={`item-${index}`} data={item} layout="vertical" />
+				))}
+			</div>
+
+			<div className="p-paris__locations__footer">
+				<Button
+					href={'/paris/locations'}
+					className="p-paris__locations__cta btn-outline"
+				>
+					View All Locations
+				</Button>
+				<span className="t-l-1 cr-subtle-5">or browse by categories:</span>
+				{locationCategories && (
+					<ul className="p-paris__locations__footer-categories">
+						{locationCategories.map((item) => (
+							<li key={`category-${item._id}`}>
+								<CategoryPill className="pill" data={item} isLink={true} />
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+		</section>
+	);
+}
--- src/app/(pages)/paris/_components/PageParis.js
+++ src/app/(pages)/paris/_components/PageParis.js
@@ -1,79 +1,17 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react';
-import useAsideMap from '@/hooks/useAsideMap';
+import React from 'react';
 import HeroSection from './HeroSection';
+import LocationsSection from './LocationsSection';
 import ItinerariesSection from './ItinerariesSection';
 import ContentListSection from './ContentListSection';
 import SeasonSection from './SeasonSection';
-import CategoryPill from '@/components/CategoryPill';
-import LocationCard from '@/components/LocationCard';
-import Button from '@/components/Button';
 
 export default function PageParis({ data }) {
-	const { locationList, locationCategories } = data || {};
-	const locationRef = useRef(null);
-	const setAsideMapActive = useAsideMap((state) => state.setAsideMapActive);
-	const setAsideMapLocations = useAsideMap(
-		(state) => state.setAsideMapLocations
-	);
-
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			([entry]) => {
-				setAsideMapActive(entry.isIntersecting);
-			},
-			{ threshold: 0.5 } // Trigger when 50% of the component is visible
-		);
-
-		if (locationRef.current) {
-			observer.observe(locationRef.current);
-		}
-
-		return () => observer.disconnect();
-	}, []);
-
-	useEffect(() => {
-		setTimeout(() => {
-			setAsideMapLocations(locationList);
-		}, 1);
-	}, [locationList]);
-
 	return (
 		<>
 			<HeroSection data={data} />
-			{locationList && (
-				<section ref={locationRef} className="p-paris__locations">
-					<div className="p-paris__locations__grid">
-						{locationList.map((item, index) => (
-							<LocationCard
-								key={`item-${index}`}
-								data={item}
-								layout="vertical"
-							/>
-						))}
-					</div>
-
-					<div className="p-paris__locations__footer">
-						<Button
-							href={'/paris/locations'}
-							className="p-paris__locations__cta btn-outline"
-						>
-							View All Locations
-						</Button>
-						<span className="t-l-1 cr-subtle-5">or browse by categories:</span>
-						{locationCategories && (
-							<ul className="p-paris__locations__footer-categories">
-								{locationCategories.map((item) => (
-									<li key={`category-${item._id}`}>
-										<CategoryPill className="pill" data={item} isLink={true} />
-									</li>
-								))}
-							</ul>
-						)}
-					</div>
-				</section>
-			)}
+			<LocationsSection data={data} />
 			<ItinerariesSection data={data} />
 			<ContentListSection data={data} />
 			<SeasonSection data={data} />
