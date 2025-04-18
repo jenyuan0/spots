a6752f52065344fa67ffc0f69b955e8afdceea61
Jen Yuan <jenyuan@me.com>
2025-04-18 18:52:34 +0200

    Image > Improve component 2

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,45 +1,55 @@
+'use client';
+
 import React, { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { buildImageSrc } from '@/lib/helpers';
 import { useInView } from 'react-intersection-observer';
 
-export default function Img({
-	image,
-	alt,
-	className,
-	responsiveImage,
-	breakpoint = 600,
-	quality = 80,
-	isAutoFormat,
-}) {
+export default function Img(props) {
+	const {
+		image,
+		alt = 'image',
+		className,
+		responsiveImage,
+		breakpoint = 600,
+		quality = 80,
+		format = 'webp',
+	} = props;
+
 	const { ref, inView } = useInView({ triggerOnce: true });
 	const [isLoaded, setIsLoaded] = useState(false);
-	const imageId = getSanityRefId(image);
-	const imageDimension = getImageDimensions(imageId);
-	const aspectRatio = image?.customRatio || imageDimension?.aspectRatio;
-	const imageWidth = imageDimension?.width;
-	const imageHeight = Math.round(imageWidth / aspectRatio);
-	const src = buildImageSrc(image, {
-		width: inView ? imageWidth : 100,
-		height: inView ? imageHeight : Math.round(100 / aspectRatio),
-		...(!isAutoFormat && { format: 'jpg' }),
-		quality,
-	});
-	const responsiveImageSrc = buildImageSrc(responsiveImage, {
-		...(!isAutoFormat && { format: 'jpg' }),
-		quality,
-	});
 	const pictureRef = useRef();
-	const [renderedDimensions, setRenderedDimensions] = useState({
-		width: 0,
-		height: 0,
-	});
+	const [renderedDimensions, setRenderedDimensions] = useState({ width: 0 });
+
+	// Get image dimensions and create src
+	const imageId = getImageId(image);
+	const dimensions = getImageDimensions(imageId);
+	const aspectRatio = image?.customRatio || dimensions?.aspectRatio;
+	const placeholderSrc = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${aspectRatio * 100} 100'%3E%3C/svg%3E`;
+	const imageWidth = dimensions?.width;
+	const imageHeight = Math.round(imageWidth / aspectRatio);
+	const imageOptions = {
+		width: imageWidth,
+		height: imageHeight,
+		format,
+		quality,
+	};
+
+	const src = renderedDimensions.width
+		? buildImageSrc(image, imageOptions)
+		: placeholderSrc;
+	const responsiveImageSrc =
+		responsiveImage &&
+		(renderedDimensions.width
+			? buildImageSrc(responsiveImage, imageOptions)
+			: placeholderSrc);
 
 	useEffect(() => {
 		if (inView && pictureRef.current) {
-			const { offsetWidth, offsetHeight } = pictureRef.current;
-			setRenderedDimensions({ width: offsetWidth, height: offsetHeight });
+			setRenderedDimensions({
+				width: pictureRef.current.offsetWidth,
+			});
 		}
 	}, [inView]);
 
@@ -67,30 +77,30 @@ export default function Img({
 				ref={ref}
 				width={imageWidth}
 				height={imageHeight}
-				sizes={inView ? `${renderedDimensions.width}px` : '0vw'}
-				src={src}
+				sizes={`${renderedDimensions.width}px`}
 				quality={quality}
 				alt={alt || image.alt || 'image'}
+				src={src}
 				onLoad={() => renderedDimensions.width > 0 && setIsLoaded(true)}
 				className={clsx({
 					lazyload: !isLoaded,
-					lazyloaded: isLoaded && inView,
+					lazyloaded: isLoaded,
 				})}
 			/>
 		</picture>
 	);
 }
 
-const getSanityRefId = (image) => {
+function getImageId(image) {
 	if (!image) return null;
 	if (typeof image === 'string') return image;
 	if (image?.asset) return image.asset._ref || image.asset._id;
 	return image._ref || image._id || null;
-};
+}
 
-const getImageDimensions = (id) => {
+function getImageDimensions(id) {
 	if (!id) return null;
 	const dimensions = id.split('-')[2];
 	const [width, height] = dimensions.split('x').map((num) => parseInt(num, 10));
 	return { width, height, aspectRatio: width / height };
-};
+}
