3aa807e32cad2a655c3c2fb4c57e36300a81a3b0
Jen Yuan <jenyuan@me.com>
2025-08-29 17:07:42 +0200

    Misc

--- src/layout/Main/index.js
+++ src/layout/Main/index.js
@@ -5,18 +5,12 @@ import clsx from 'clsx';
 import { pageTransitionFade } from '@/lib/animate';
 import useAsideMap from '@/hooks/useAsideMap';
 
-export default function Main({ children, isSpaceL, isSpaceR }) {
+export default function Main({ children }) {
 	const pathname = usePathname();
-	const { asideMapExpand } = useAsideMap();
 
 	return (
 		<motion.main
 			id="main"
-			className={clsx({
-				'is-space-l': isSpaceL,
-				'is-space-r': isSpaceR,
-				'is-aside-map-expand': asideMapExpand,
-			})}
 			key={pathname}
 			initial="initial"
 			animate="animate"
