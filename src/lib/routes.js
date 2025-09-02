749fc2ef8cc2dc5621bd7d04bcceea887844a223
Jen Yuan <jenyuan@me.com>
2025-09-03 00:03:12 +0200

    Misc

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -45,7 +45,7 @@ export const getRoute = ({ documentType, slug }) => {
 		case 'gItineraries':
 			return `/paris/itinerary/${slug}`;
 		case 'gCases':
-			return `/cases?m=${slug}&t=case`;
+			return `/travel-design?m=${slug}&t=case`;
 
 		case 'externalUrl':
 			return slug;
