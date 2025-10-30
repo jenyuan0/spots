a6afb90fff7bc87310a4a74ca89fdae0e41a8c47
Jen Yuan <jenyuan@me.com>
2025-10-30 23:39:46 +0100

    Fix

--- src/app/(pages)/travel-design/_components/PageCasesSingle.js
+++ src/app/(pages)/travel-design/_components/PageCasesSingle.js
@@ -1,8 +1,12 @@
 'use client';
 
-import React,  from 'react';
+import React from 'react';
 import MagnifyCaseEl from '@/layout/Magnify/MagnifyCaseEl';
 
 export default function PageCasesSingle({ data }) {
-	return <div className="p-case-single"><MagnifyCaseEl data={data} /></div>;
+	return (
+		<div className="p-case-single">
+			<MagnifyCaseEl data={data} />
+		</div>
+	);
 }
